4d5b2b8091b429a9cf33cb314c9c6aaf89fccd87
hazharaziz <hazharaziz5555@gmail.com>
2021-02-11 15:45:51 +0330

    scaffold database context and models

---
+++ back-end/WebServer/Models/Follower.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace WebServer.Models
+{
+    [Index(nameof(UserId), nameof(FollowerId), Name = "UNIQUE_RELATION", IsUnique = true)]
+    public partial class Follower
+    {
+        [Key]
+        [Column("ID")]
+        public int Id { get; set; }
+        [Column("UserID")]
+        public int UserId { get; set; }
+        [Column("FollowerID")]
+        public int FollowerId { get; set; }
+        public int? Pending { get; set; }
+
+        [ForeignKey(nameof(FollowerId))]
+        [InverseProperty("FollowerFollowerNavigations")]
+        public virtual User FollowerNavigation { get; set; }
+        [ForeignKey(nameof(UserId))]
+        [InverseProperty("FollowerUsers")]
+        public virtual User User { get; set; }
+    }
+}
---
+++ back-end/WebServer/Models/Message.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace WebServer.Models
+{
+    public partial class Message
+    {
+        public Message()
+        {
+            InverseReplyTo = new HashSet<Message>();
+        }
+
+        [Key]
+        [Column("ID")]
+        public int Id { get; set; }
+        [Column("ComposerID")]
+        public int? ComposerId { get; set; }
+        [Column("ReplyToID")]
+        public int? ReplyToId { get; set; }
+        [Required]
+        [StringLength(400)]
+        public string Text { get; set; }
+        [Required]
+        [StringLength(70)]
+        public string ComposerName { get; set; }
+        [Column(TypeName = "datetime")]
+        public DateTime DateTime { get; set; }
+
+        [ForeignKey(nameof(ComposerId))]
+        [InverseProperty(nameof(User.Messages))]
+        public virtual User Composer { get; set; }
+        [ForeignKey(nameof(ReplyToId))]
+        [InverseProperty(nameof(Message.InverseReplyTo))]
+        public virtual Message ReplyTo { get; set; }
+        [InverseProperty(nameof(Message.ReplyTo))]
+        public virtual ICollection<Message> InverseReplyTo { get; set; }
+    }
+}
