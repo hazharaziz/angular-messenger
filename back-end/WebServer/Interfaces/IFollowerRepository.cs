fc04c17060c7bbfda776fa4d8138866121298c04
hazharaziz <hazharaziz5555@gmail.com>
2021-02-12 21:55:20 +0330

    add follower repository interface

---
+++ back-end/WebServer/Interfaces/IFollowerRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using WebServer.Models;
+
+namespace WebServer.Interfaces
+{
+    interface IFollowerRepository
+    {
+        IEnumerable<Follower> GetFollowers(int userId);
+        IEnumerable<Follower> GetAll();
+        IEnumerable<Follower> Find(Expression<Func<Follower, bool>> predicate);
+        void Add(Follower entity);
+        void AddRange(IEnumerable<Follower> entities);
+        void Remove(Follower entity);
+        void RemoveRange(IEnumerable<Follower> entities);
+        bool HasFollower(int userId, int followerId);
+        bool HasRequestFrom(int userId, int followerId);
+    }
+}
