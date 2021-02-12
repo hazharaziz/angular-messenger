4f106d3aab86da8198bad51f3d4c7b3860bb08ef
hazharaziz <hazharaziz5555@gmail.com>
2021-02-12 18:05:30 +0330

    init auth controller

---
+++ back-end/WebServer/Controllers/AuthenticationController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebServer.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private DbContext _messageContext;
+
+        public AuthenticationController(DbContext messageContext)
+        {
+            _messageContext = messageContext;
+        }
+    }
+}
