6de0224c38de581e60ce9471ebf410414bd1f57c
hazharaziz <hazharaziz5555@gmail.com>
2021-02-12 21:53:51 +0330

    add message repository interface

---
+++ back-end/WebServer/Interfaces/IMessageRespository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using WebServer.Models;
+
+namespace WebServer.Interfaces
+{
+    interface IMessageRespository
+    {
+        Message Get(int id);
+        IEnumerable<Message> GetAll();
+        IEnumerable<Message> Find(Expression<Func<Message, bool>> predicate);
+        void Add(Message entity);
+        void AddRange(IEnumerable<Message> entities);
+        void Remove(Message entity);
+        void RemoveRange(IEnumerable<Message> entities);
+    }
+}
