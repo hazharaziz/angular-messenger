8b84b6cb13b4c41e1aa697d4f2a115418c1e5cc1
hazharaziz <hazharaziz5555@gmail.com>
2021-02-12 22:09:07 +0330

    implement message repository class

---
+++ back-end/WebServer/Repositories/MessageRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using WebServer.DataContext;
+using WebServer.Interfaces;
+using WebServer.Models;
+
+namespace WebServer.Repositories
+{
+    public class MessageRepository : IMessageRepository
+    {
+        public MessengerContext Context;
+
+        public MessageRepository(MessengerContext messengerContext)
+        {
+            Context = messengerContext;
+        }
+
+        public Message Get(int id)
+            => Context.Set<Message>().Find(id);
+
+        public IEnumerable<Message> GetAll()
+            => Context.Set<Message>().ToList();
+
+        public IEnumerable<Message> Find(Expression<Func<Message, bool>> predicate)
+            => Context.Set<Message>().Where(predicate);
+
+        public void Add(Message entity)
+            => Context.Set<Message>().Add(entity);
+
+        public void AddRange(IEnumerable<Message> entities)
+            => Context.Set<Message>().AddRange(entities);
+
+        public void Remove(Message entity)
+            => Context.Set<Message>().Remove(entity);
+
+        public void RemoveRange(IEnumerable<Message> entities)
+            => Context.Set<Message>().RemoveRange(entities);
+    }
+}
