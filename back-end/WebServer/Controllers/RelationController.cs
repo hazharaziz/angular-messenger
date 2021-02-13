71d39ff8a24645094853b14e601251fa34d1a408
hazharaziz <hazharaziz5555@gmail.com>
2021-02-13 18:44:22 +0330

    init relation api controller

---
+++ back-end/WebServer/Controllers/RelationController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebServer.Interfaces;
+
+namespace WebServer.Controllers
+{
+    [Route("api/relations")]
+    [ApiController]
+    public class RelationController : ControllerBase
+    {
+        private IRelationAPI _relations;
+
+        public RelationController(IRelationAPI relations)
+        {
+            _relations = relations;
+        }
+    }
+}
