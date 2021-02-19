229658d63ed35119ca21c96d7ee9dc7f7148dffd
hazharaziz <hazharaziz5555@gmail.com>
2021-02-19 16:28:54 +0330

    add group service interface, add group response model

---
+++ back-end/WebServer/Interfaces/IGroupService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebServer.Models.DBModels;
+using WebServer.Models.ResponseModels;
+
+namespace WebServer.Interfaces
+{
+    public interface IGroupService
+    {
+        Response<List<GroupModel>> GetUserGroups(int userId);
+        Response<Group> GetGroupInfo(int groupId);
+        Response<string> CreateGroup(Group group);
+        Response<string> EditGroup(int userId, Group editedGroup);
+        Response<string> DeleteGroup(int userId, int groupId);
+        Response<string> AddMembersToGroup(int groupId, List<int> memberIds);
+        Response<string> RemoveMemberFromGroup(int groupId, int memberId);
+        Response<string> SendGroupMessage(int userId, int groupId, GroupMessage message);
+        Response<string> EditMessage(int userId, int groupId, GroupMessage editedMessage);
+        Response<string> DeleteMessage(int userId, int messageId);
+        Response<string> ClearGroupHistory(int userId, int groupId);
+        Response<string> LeaveGroup(int userId, int groupId);
+    }
+}
