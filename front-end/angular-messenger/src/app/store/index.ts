bf924d9c8890adf051ba56f0c8425a2763d334d6
hazharaziz <hazharaziz5555@gmail.com>
2021-03-06 23:02:23 +0330

    implement get general chat messages reducer

--- front-end/angular-messenger/src/app/store/index.ts
+++ front-end/angular-messenger/src/app/store/index.ts
@@ -1,17 +1,22 @@
 import { User } from '../models/data/user.model';
 
 import * as fromAuthReducer from './reducers/auth.reducer';
+import * as fromChatReducer from './reducers/chat.reducer';
 import { ActionReducerMap, MetaReducer } from '@ngrx/store';
 import { environment } from 'src/environments/environment';
+import { Chat } from '../models/data/chat.model';
 
 export const userStateFeatureKey = 'user';
+export const generalChatStateFeatureKey = 'chat';
 
 export type AppState = {
   [userStateFeatureKey]: User;
+  [generalChatStateFeatureKey]: Chat[];
 };
 
 export const reducers: ActionReducerMap<AppState> = {
-  [userStateFeatureKey]: fromAuthReducer.authReducer
+  [userStateFeatureKey]: fromAuthReducer.authReducer,
+  [generalChatStateFeatureKey]: fromChatReducer.chatReducer
 };
 
 export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [] : [];
