7c2baf4151fd54aca89f40b1e53c9602019fcf40
hazharaziz <hazharaziz5555@gmail.com>
2021-03-09 23:17:58 +0330

    implement change password api service

--- front-end/angular-messenger/src/app/models/interfaces/profile.api.ts
+++ front-end/angular-messenger/src/app/models/interfaces/profile.api.ts
@@ -4,4 +4,5 @@ import { User } from '../data/user.model';
 export interface ProfileAPI {
   getProfileRequest(): Observable<User>;
   editProfileRequest(editedUser: User): Observable<string>;
+  changePasswordRequest(oldPassword: string, newPassword: string): Observable<string>;
 }
