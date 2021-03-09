7687469c1a3596bfd407576775e5368bf78e2a65
hazharaziz <hazharaziz5555@gmail.com>
2021-03-09 10:06:13 +0330

    add authentication model

--- front-end/angular-messenger/src/app/screens/profile/profile.component.ts
+++ front-end/angular-messenger/src/app/screens/profile/profile.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { User } from 'src/app/models/data/user.model';
+import { AppState } from 'src/app/store';
+import { AuthSelectors } from 'src/app/store/selectors/auth.selectors';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +11,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  constructor(private store: Store<AppState>) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
