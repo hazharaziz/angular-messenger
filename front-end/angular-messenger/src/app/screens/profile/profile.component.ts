0af36cc991371838ea93cf1e8bf0afc45f3bdd27
hazharaziz <hazharaziz5555@gmail.com>
2021-03-09 11:24:12 +0330

    integrate profile actions and observable with profile component

--- front-end/angular-messenger/src/app/screens/profile/profile.component.ts
+++ front-end/angular-messenger/src/app/screens/profile/profile.component.ts
@@ -3,7 +3,8 @@ import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { User } from 'src/app/models/data/user.model';
 import { AppState } from 'src/app/store';
-import { AuthSelectors } from 'src/app/store/selectors/auth.selectors';
+import { ProfileActions } from 'src/app/store/actions/profile.actions';
+import { ProfileSelectors } from 'src/app/store/selectors/profile.selectors';
 
 @Component({
   selector: 'app-profile',
@@ -11,7 +12,15 @@ import { AuthSelectors } from 'src/app/store/selectors/auth.selectors';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  profile$: Observable<User> = this.store.select(ProfileSelectors.selectUser);
+
   constructor(private store: Store<AppState>) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.fetchProfile();
+  }
+
+  fetchProfile(): void {
+    this.store.dispatch(ProfileActions.GetProfileRequest());
+  }
 }
