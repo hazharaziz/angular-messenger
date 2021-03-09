a9c15cb5310eabea1c744ea18d34b007fe6b623c
hazharaziz <hazharaziz5555@gmail.com>
2021-03-09 10:19:46 +0330

    add profile actions, implement get profile actions

---
+++ front-end/angular-messenger/src/app/store/actions/profile.actions.ts
@@ -0,0 +1,10 @@
+import { createAction, props } from '@ngrx/store';
+
+import { User } from 'src/app/models/data/user.model';
+import { ActionTypes } from './types';
+
+export const ProfileActions = {
+  GetProfileRequest: createAction(ActionTypes.GetProfileRequest),
+  GetProfileSuccess: createAction(ActionTypes.GetProfileSuccess, props<User>()),
+  GetProfileFail: createAction(ActionTypes.GetProfileSuccess, props<{ error: string }>())
+};
