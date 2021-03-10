d6a46652c7f4a3be27222226277f0c69ada40fcf
hazharaziz <hazharaziz5555@gmail.com>
2021-03-10 14:35:52 +0330

    implement auth guard

--- front-end/angular-messenger/src/app/app-routing.module.ts
+++ front-end/angular-messenger/src/app/app-routing.module.ts
@@ -11,15 +11,16 @@ import { PageNotFoundComponent } from './screens/page-not-found/page-not-found.c
 import { ProfileComponent } from './screens/profile/profile.component';
 import { SearchComponent } from './screens/search/search.component';
 import { SignupComponent } from './screens/signup/signup.component';
+import { AuthGuard } from './services/guards/auth.guard';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'sign-up', component: SignupComponent },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'general', component: GeneralComponent },
-  { path: 'search', component: SearchComponent },
-  { path: 'direct', component: DirectComponent },
-  { path: 'group', component: GroupComponent },
+  { path: 'general', component: GeneralComponent, canActivate: [AuthGuard] },
+  { path: 'search', component: SearchComponent, canActivate: [AuthGuard] },
+  { path: 'direct', component: DirectComponent, canActivate: [AuthGuard] },
+  { path: 'group', component: GroupComponent, canActivate: [AuthGuard] },
   {
     path: 'profile',
     component: ProfileComponent,
